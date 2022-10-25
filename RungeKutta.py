14a41b08a9784038a8b6a103d63ff60c2793a5a2
Cathewrine Brower <catherinemb42@gmail.com>
2022-10-25 10:33:37 -0400

    adding kcore code

--- RungeKutta.py
+++ RungeKutta.py
@@ -24,22 +24,14 @@ class RK4(object):
 
 
     def _solve(self, y, t, h):
-        functions = self.f
-
         k1 = []
-        for f in functions:
-            k1.append(h * f(t, *y))
-
         k2 = []
-        for f in functions:
-            k2.append(h * f(t + .5*h, *[y[i] + .5*h*k1[i] for i in range(0, len(y))]))
-
         k3 = []
-        for f in functions:
-            k3.append(h * f(t + .5*h, *[y[i] + .5*h*k2[i] for i in range(0, len(y))]))
-
         k4 = []
-        for f in functions:
+        for f in self.f:
+            k1.append(h * f(t, *y))
+            k2.append(h * f(t + .5*h, *[y[i] + .5*h*k1[i] for i in range(0, len(y))]))
+            k3.append(h * f(t + .5*h, *[y[i] + .5*h*k2[i] for i in range(0, len(y))]))
             k4.append(h * f(t + h, *[y[i] + h*k3[i] for i in range(0, len(y))]))
 
         return [y[i] + (k1[i] + 2*k2[i] + 2*k3[i] + k4[i]) / 6.0 for i in range(0, len(y))]
